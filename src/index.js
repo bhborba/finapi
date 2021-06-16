2694793d82bfd6fd855a41b98a40332151088e21
bhborba <brunohenriqueborba@gmail.com>
2021-06-16 11:45:47 -0300

    Middlewares

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next){
+    const {cpf} = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({error: "Customer not found"}); 
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body;
 
@@ -28,14 +43,10 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-    const {cpf} = request.headers;
+// app.use(verifyIfExistsAccountCPF);
 
-    const customer = customers.find(customer => customer.cpf === cpf);
-
-    if(!customer) {
-       return response.status(400).json({error: "Customer not found"}); 
-    }
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
 
     return response.json(customer.statemant);
 })
