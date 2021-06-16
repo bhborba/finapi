e21755e59e494d1027e933f11dabe9620a9433b4
bhborba <brunohenriqueborba@gmail.com>
2021-06-16 13:17:24 -0300

    Creating withdraw

--- src/index.js
+++ src/index.js
@@ -22,6 +22,18 @@ function verifyIfExistsAccountCPF(request, response, next){
     return next();
 }
 
+function getBalance(statement){
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit'){
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body;
 
@@ -37,7 +49,7 @@ app.post("/account", (request, response) => {
         cpf,
         name,
         id: uuidv4(),
-        statemant: []
+        statement: []
     });
 
     return response.status(201).send();
@@ -48,7 +60,7 @@ app.post("/account", (request, response) => {
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
     const { customer } = request;
 
-    return response.json(customer.statemant);
+    return response.json(customer.statement);
 });
 
 app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
@@ -63,9 +75,30 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
         type: "credit"
     }
 
-    customer.statemant.push(statementOperation);
+    customer.statement.push(statementOperation);
 
     return response.status(201).send();
 });
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount) {
+        return response.status(400).json({error: "Insufficient funds!"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
 
 app.listen(3333);
