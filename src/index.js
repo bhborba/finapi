b8606041272bc16a95526f47f0e5f1b07c0f37da
bhborba <brunohenriqueborba@gmail.com>
2021-06-16 09:52:50 -0300

    Listing statement

--- src/index.js
+++ src/index.js
@@ -26,6 +26,14 @@ app.post("/account", (request, response) => {
     });
 
     return response.status(201).send();
+});
+
+app.get("/statement/:cpf", (request, response) => {
+    const {cpf} = request.params;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    return response.json(customer.statemant);
 })
 
 app.listen(3333);
