5b7ba3218c2e527f9183f6f274db76582bd7571d
bhborba <brunohenriqueborba@gmail.com>
2021-06-16 13:36:37 -0300

    Update account

--- src/index.js
+++ src/index.js
@@ -114,4 +114,19 @@ app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
     return response.json(customer.statement);
 });
 
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send();
+})
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer);
+})
+
 app.listen(3333);
